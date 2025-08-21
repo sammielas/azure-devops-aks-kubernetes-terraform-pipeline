3de083737ad80eaf13093128290cd65e2ccb804d
Samson Lasode <samflicks@gmail.com>
2025-08-21 18:08:38 +0100

    set azure_rbac_enabled = true

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
   # Integrate Azure AD for RBAC
   azure_active_directory_role_based_access_control {
-    managed                = true
+    azure_rbac_enabled     = true
     admin_group_object_ids = [azuread_group.aks_administrators.object_id]
   }
 
